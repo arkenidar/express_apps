775ce9a63c646c51e3d29196294c2deacda82214
arkenidar <dario.cangialosi@gmail.com>
2016-06-16 15:35:33 +0200

    knex query-builder

--- db/users.js
+++ db/users.js
@@ -3,10 +3,13 @@ var config = require('../config.json');
 var pg = require('pg');
 var DATABASE_URL = process.env.DATABASE_URL || config.DATABASE_URL;
 
+var knex = require('knex')({client: 'pg'});
+
 exports.findById = function(id, cb) {
 process.nextTick(function() {
   pg.connect(DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM users WHERE id='+id, function(err, result) {
+  var sql = knex('users').where('id','=',id).toString();
+  client.query(sql, function(err, result) {
     done();
     if (err){
       console.error(err);
@@ -27,7 +30,8 @@ exports.findByUsername = function(username, cb) {
 process.nextTick(function() {
   
   pg.connect(DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM users WHERE username=\''+username+'\'', function(err, result) {
+  var sql = knex('users').where('username','=',username).toString();
+  client.query(sql, function(err, result) {
     done();
     if (err){
       console.error(err);
